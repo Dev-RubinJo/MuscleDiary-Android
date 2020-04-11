ee2268eecf6e1803cca224500314b593a8c72193
hsj321 <randy3456@naver.com>
2020-04-11 23:49:33 +0900

    1. ChartFragment 리사이클러뷰 더미데이터 적용 함수 작성 2. ChartFragment 리사이클러뷰 적용 코드 작성 3. ChartAdapter 생성, 작성 4. ChartItem 생성, 작성 4. fragment_chart 리사이클러뷰 id지정

---
+++ MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/chart/ChartAdapter.java
@@ -0,0 +1,53 @@
+package com.munchkin.musclediary.src.main.chart;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.munchkin.musclediary.R;
+
+import java.util.ArrayList;
+
+public class ChartAdapter extends RecyclerView.Adapter<ChartAdapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<ChartItem> mItems;
+
+    ChartAdapter(Context context, ArrayList<ChartItem> items){
+        mContext = context;
+        mItems = items;
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvLevel;
+        TextView tvDate;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvLevel = itemView.findViewById(R.id.tv_analysis_chart_item);
+            tvDate = itemView.findViewById(R.id.tv_date_chart_item);
+        }
+    }
+
+    @Override
+    public ChartAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_chart, parent, false);
+        ChartAdapter.ViewHolder viewHolder = new ChartAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(ChartAdapter.ViewHolder holder, int position) {
+        ChartItem item = mItems.get(position);
+        holder.tvLevel.setText(item.getLevel());
+        holder.tvDate.setText(item.getDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+}
--- MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/chart/ChartFragment.java
+++ MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/chart/ChartFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
@@ -22,12 +25,26 @@ import java.util.List;
 public class ChartFragment extends BaseFragment {
 
     private LineChart lineChart;
+    //리사이클러뷰 아이템 리스트
+    private ArrayList<ChartItem> items;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         final View view = inflater.inflate(R.layout.fragment_chart, container, false);
         setComponentView(view);
 
+        //더미데이터 넣는 함수 실행
+        addRecyclerList();
+
+        //리사이클러뷰 생성, 레이아웃 매니저 적용
+        RecyclerView chartRecyclerView = view.findViewById(R.id.recycler_chart);
+        chartRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        //리사이클러뷰 어뎁터 생성, 적용
+        ChartAdapter adapter = new ChartAdapter(getContext(), items);
+        chartRecyclerView.setAdapter(adapter);
+
+        //예시용 그래프 라이브러리
         lineChart = (LineChart)view.findViewById(R.id.line_chart);
 
         List<Entry> entries = new ArrayList<>();
@@ -79,5 +96,20 @@ public class ChartFragment extends BaseFragment {
     @Override
     public void setComponentView(View v) {
 
+    }
+
+    //리사이클러뷰 리스트 아이템 채우는 함수
+    private void addRecyclerList(){
+        //더미데이터들
+        String levelList[] = {"75.5Kg", "74.5Kg","75.7Kg","74.9Kg","75.1Kg","74.5Kg"};
+        String dateList[] = {"2020년 04월 9일", "2020년 04월 10일","2020년 04월 11일","2020년 04월 12일","2020년 04월 13일","2020년 04월 14일"};
+        items = new ArrayList<>();
+
+        for(int i = 0; i < levelList.length; i++){
+            ChartItem item = new ChartItem();
+            item.setLevel(levelList[i]);
+            item.setDate(dateList[i]);
+            items.add(item);
+        }
     }
 }
