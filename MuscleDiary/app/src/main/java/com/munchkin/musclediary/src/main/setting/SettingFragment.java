c8fc4e2add5a60b586c310d133c2dcd77db946f8
hsj321 <randy3456@naver.com>
2020-04-23 04:44:20 +0900

    1. 설정화면 끼니추가 버튼 생성 2. 설정화면 끼니추가 버튼 리스너 생성, 적용 3. 끼니추가 팝업 화면  생성, 적용 4. 끼니추가 팝업 화면 버튼 리스너 생성, 적용 5. 끼니추가 팝업 화면 배경 클릭시 뒤로가기 적용

--- MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/setting/SettingFragment.java
+++ MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/setting/SettingFragment.java
@@ -1,21 +1,27 @@
 package com.munchkin.musclediary.src.main.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.munchkin.musclediary.R;
 import com.munchkin.musclediary.src.BaseFragment;
-import com.munchkin.musclediary.src.main.chart.ChartAdapter;
-import com.munchkin.musclediary.src.main.chart.ChartItem;
 
 import java.util.ArrayList;
 
+import static android.app.Activity.RESULT_OK;
+
 public class SettingFragment extends BaseFragment {
+
+    //startActivityForResult requestCode
+    private final int ADD_MEAL = 0;
 
     private ArrayList<SettingItem> mItems;
 
@@ -30,6 +36,12 @@ public class SettingFragment extends BaseFragment {
     @Override
     public void setComponentView(View v) {
 
+        //클릭 리스너 생성
+        View.OnClickListener listener = onClickListener();
+
+        //끼니 추가 버튼
+        Button btAdd = v.findViewById(R.id.bt_add_setting);
+        btAdd.setOnClickListener(listener);
 
         //더미데이터 넣는 함수 실행
         addRecyclerList();
@@ -43,6 +55,26 @@ public class SettingFragment extends BaseFragment {
         chartRecyclerView.setAdapter(adapter);
     }
 
+    //클릭 리스너 모음
+    private View.OnClickListener onClickListener(){
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    //끼니 추가 버튼 클릭이벤트
+                    case R.id.bt_add_setting:
+                        Intent intent = new Intent(getActivity(), InputSettingActivity.class);
+                        startActivityForResult(intent, ADD_MEAL);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
+        return listener;
+    }
+
+
     //리사이클러뷰 리스트 아이템 채우는 함수
     private void addRecyclerList(){
         //더미데이터들
@@ -54,5 +86,20 @@ public class SettingFragment extends BaseFragment {
             item.setMeal(meal);
             mItems.add(item);
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode != RESULT_OK){
+            return;
+        }
+        switch(requestCode){
+            case ADD_MEAL:
+                //추가했을 때 리스트에 추가하는 코드 필요
+                break;
+            default:
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
