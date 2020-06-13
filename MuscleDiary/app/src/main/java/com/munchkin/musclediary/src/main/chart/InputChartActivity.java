088e298b3e9c8f88a681e636d8233c278bb7ccc7
hsj321 <randy3456@naver.com>
2020-06-14 03:30:51 +0900

    1. 기록 화면 몸무게 추가 팝업창 날짜 UI변경 2. 기록화면 몸무게 날짜 제한 삭제

--- MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/chart/InputChartActivity.java
+++ MuscleDiary/app/src/main/java/com/munchkin/musclediary/src/main/chart/InputChartActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.DatePicker;
 import android.widget.NumberPicker;
 
 import com.munchkin.musclediary.R;
@@ -20,9 +21,13 @@ import java.util.List;
 public class InputChartActivity extends BaseActivity {
     private Button mBtnBack;
     private Button mBtAdd;
-    private NumberPicker mDatePicker;
+    private DatePicker mDatePicker;
     private NumberPicker mIntegerPicker;
     private NumberPicker mFloatPicker;
+
+    private int mYear = 0;
+    private int mMonth = 0;
+    private int mDate = 0;
 
     private String mDates[];
 
@@ -34,9 +39,12 @@ public class InputChartActivity extends BaseActivity {
         //컴포넌트 연결
         mBtnBack = findViewById(R.id.input_chart_btn_cancle);
         mBtAdd = findViewById(R.id.input_chart_btn_complete);
-        mDatePicker = (NumberPicker) findViewById(R.id.input_chart_date_picker);
+        mDatePicker = (DatePicker) findViewById(R.id.date_picker_input_chart);
         mIntegerPicker = (NumberPicker) findViewById(R.id.input_chart_integer_picker);
         mFloatPicker = (NumberPicker) findViewById(R.id.input_chart_float_picker);
+
+        //현재 날짜 설정
+        setDate();
 
         //listener 등록
         this.initOnClickListener();
@@ -58,13 +66,12 @@ public class InputChartActivity extends BaseActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 Calendar c1 = Calendar.getInstance();
-                int year = c1.get(Calendar.YEAR);
-                String dayMonth = mDates[mDatePicker.getValue()];
-                int month = Integer.parseInt(dayMonth.substring(0,2));
-                int date = Integer.parseInt(dayMonth.substring(5,7));
-                intent.putExtra("year", year) ;
-                intent.putExtra("month", month);
-                intent.putExtra("date", date);
+                mYear = mDatePicker.getYear();
+                mMonth = mDatePicker.getMonth() +1;
+                mDate = mDatePicker.getDayOfMonth();
+                intent.putExtra("year", mYear) ;
+                intent.putExtra("month", mMonth);
+                intent.putExtra("date", mDate);
                 intent.putExtra("weight", mIntegerPicker.getValue() * 1.0f + mFloatPicker.getValue() * 0.1f);
                 setResult(RESULT_OK, intent);
                 finish();
@@ -73,23 +80,14 @@ public class InputChartActivity extends BaseActivity {
     }
 
     private void initNumberPicker() {
-        mDatePicker.setMinValue(0);
-        mDatePicker.setMaxValue(mDates.length-1);
-        mDatePicker.setFormatter(new NumberPicker.Formatter() {
-            @Override
-            public String format(int value) {
-                return mDates[value];
-            }
-        });
-        mDatePicker.setWrapSelectorWheel(false);
-        mDatePicker.setOnLongPressUpdateInterval(100);
-        mDatePicker.setDisplayedValues(mDates);
+        mDatePicker.init(mYear, mMonth-1, mDate, mOnDateChangedListener);
+        mDatePicker.setMaxDate(System.currentTimeMillis());
 
         mIntegerPicker.setMinValue(0);
         mIntegerPicker.setMaxValue(120);
         mIntegerPicker.setWrapSelectorWheel(false);
         mIntegerPicker.setOnLongPressUpdateInterval(100);
-        mIntegerPicker.setValue(70);
+        mIntegerPicker.setValue(30);
 
         mFloatPicker.setMinValue(0);
         mFloatPicker.setMaxValue(9);
@@ -97,18 +95,39 @@ public class InputChartActivity extends BaseActivity {
         mFloatPicker.setOnLongPressUpdateInterval(100);
     }
 
+    private DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener(){
+        @Override
+        public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
+            mYear = yy;
+            mMonth = mm;
+            mDate = dd;
+        }
+    };
+
+    private void setDate(){
+        Calendar c1 = Calendar.getInstance();
+        mYear = c1.get(Calendar.YEAR);
+        mMonth = c1.get(Calendar.MONTH) + 1;
+        mDate = c1.get(Calendar.DATE);
+    }
+
     private String[] getDatesFromCalender() {
         Calendar c1 = Calendar.getInstance();
+        mYear = c1.get(Calendar.YEAR);
+        mMonth = c1.get(Calendar.MONTH) + 1;
+        mDate = c1.get(Calendar.DATE);
 
         List<String> dates = new ArrayList<String>();
         DateFormat dateFormat = new SimpleDateFormat("MM / dd (EE)");
         dates.add(dateFormat.format(c1.getTime()));
 
 
-        for (int i = 0; i < 60; i++) {
+        for (int i = 0; i < 2000; i++) {
             c1.add(Calendar.DATE, -1);
             dates.add(dateFormat.format(c1.getTime()));
         }
+
+
 
 //      미래까지 필요시
 //        for (int i = 0; i < 60; i++) {
@@ -120,4 +139,6 @@ public class InputChartActivity extends BaseActivity {
 
         return dates.toArray(new String[dates.size() - 1]);
     }
+
+
 }
